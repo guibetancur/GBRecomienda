42fd0f122652ca1834e767e5163c90a9a14091f4
Guillermo Betancur <guibetancur@gmail.com>
2017-09-04 19:04:59 -0500

    Cambio de la ruta a index (recommendations)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: "recommendations#index"
+  get 'recommendations', to: 'recommendations#index'
+
   resources :recommendations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
